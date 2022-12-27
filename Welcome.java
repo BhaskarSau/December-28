4fb9a4c8d9ae40cb30bf7c0b56f6c2d5b07e9ce8
BhaskarSau <116186389+BhaskarSau@users.noreply.github.com>
2022-12-27 16:31:38 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+public class Welcome{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Code With Bhaskar!");
+        System.out.println("Anonymous!");
+        for(int i=0; i<10; i++){
+            for(int j=0; j<i;j++){
+                System.out.print("Sorry"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
